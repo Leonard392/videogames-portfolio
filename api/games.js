d0776f3c1832f1c35022ef645c9743e6505b0de6
Leonard392 <leorp392@gmail.com>
2025-11-15 17:27:20 -0500

    update

---
+++ api/games.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch("https://www.freetogame.com/api/games");
+    const data = await response.json();
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Error en el proxy" });
+  }
+}
